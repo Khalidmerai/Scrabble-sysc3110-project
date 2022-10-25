b33d20acb222d07289c9f9f51fb438a9ab0a7ca4
scsc2022 <schararaislam13@gmail.com>
2022-10-25 16:36:40 -0400

    Updated

--- Dictionary.java
+++ Dictionary.java
@@ -1,14 +1,22 @@
-//Scharara islam 101149731
+//Scharara Islam 101149731
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Author: Scharara Islam 101149731 and Khalid Merai 101159203
+ * This class is to import the list of word that is acceptable in the Scrabble game.
+ * The link is provided on the project description and downloaded as a .txt file
+ */
 public class Dictionary {
     private ArrayList<String> dictionaryList = new ArrayList<String>();
 
     private static final String wordList = "wordlist.10000.txt";
-//construct the dictionary and import text file
+
+    /**
+     * construct the dictionary and import text file
+     */
     public Dictionary() {
         Scanner filename = null;
         boolean verified;
@@ -24,7 +32,11 @@ public class Dictionary {
         }
     }
 
-//checks if the word contains the list that is provided
+    /**
+     * Check if the word from the player matches the dictionary list
+     * @param wordContain
+     * @return  true if the word contains the list that is provided, otherwise false
+     */
     public boolean checkWord(String wordContain){
         if (wordContain.length() == 1){
             return true;
