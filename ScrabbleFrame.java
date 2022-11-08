605025d70fcf57c23aa68748aac687e7ef766ecb
saad_ <saadeid@cmail.carleton.ca>
2022-11-07 20:38:49 -0500

    Implemented more GUI features such as menu bar and panels.

--- ScrabbleFrame.java
+++ ScrabbleFrame.java
@@ -18,16 +18,77 @@ public class ScrabbleFrame extends JFrame implements ScrabbleView{
     /** The squares on the board. */
     public JButton[][] buttons;
 
+    /**
+     * Initalizes the different panels within the frame
+     */
+    private JPanel gameInfoPanel, gridPanel, rackPanel;
+
     public ScrabbleFrame(){
         super("Scrabble");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(numRows,numColumns));
+        this.setLayout(null);
         buttons = new JButton[numRows][numColumns];
 
-        buildGuiSquares();
+        buildPanels();
+        buildMenuBar();
 
-        this.setSize(500,500);
+        this.setSize(610,650);
         this.setVisible(true);
+    }
+
+    /** Incomplete! Will be complete in MileStone 4.
+     * This method adds the menu items to the menu bar.
+     */
+    private void buildMenuBar(){
+        ScrabbleModel model = new ScrabbleModel();
+        model.addScrabbleView(this);
+        ScrabbleController controller = new ScrabbleController(model);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        menu.addActionListener(controller);
+
+        JMenuItem item = new JMenuItem("Undo");
+        item.addActionListener(controller);
+        menu.add(item);
+
+        item = new JMenuItem("Save");
+        item.addActionListener(controller);
+        menu.add(item);
+
+        item = new JMenuItem("Load");
+        item.addActionListener(controller);
+        menu.add(item);
+
+        menuBar.add(menu);
+        this.setJMenuBar(menuBar);
+    }
+
+    /**
+     * Creates the JPanels needed to showcase the player's game info, grid, and letter rack.
+     */
+    private void buildPanels(){
+        gameInfoPanel = new JPanel();
+        gridPanel = new JPanel();
+        rackPanel = new JPanel();
+
+        gameInfoPanel.setLayout(new BorderLayout());
+        gridPanel.setLayout(new GridLayout(15,15));
+        rackPanel.setLayout(new BorderLayout());
+
+        gameInfoPanel.setBounds(10,10,580,30);
+        gridPanel.setBounds(10, 50,580,500);
+        rackPanel.setBounds(10,560,580,50);
+
+        //The background colours are to signify the
+        // location of the panels and will be removed once the info is added
+        gameInfoPanel.setBackground(Color.YELLOW);
+        buildGuiSquares();
+        rackPanel.setBackground(Color.BLUE);
+
+        this.add(gameInfoPanel);
+        this.add(gridPanel);
+        this.add(rackPanel);
     }
 
     /**
@@ -44,7 +105,7 @@ public class ScrabbleFrame extends JFrame implements ScrabbleView{
                 b.setActionCommand(i + " " + j);
                 buttons[i][j] = b;
                 b.addActionListener(controller);
-                this.add(b);
+                gridPanel.add(b);
             }
         }
     }
