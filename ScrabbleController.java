019533c44ae67b656ec56c7d5c1c86c0729331a9
marinalatif1 <114357108+marinalatif1@users.noreply.github.com>
2022-11-08 21:29:19 -0500

    Update ScrabbleController.java

--- ScrabbleController.java
+++ ScrabbleController.java
@@ -12,7 +12,22 @@ public class ScrabbleController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         String[] input = e.getActionCommand().split(" ");
-        int x = Integer.parseInt(input[0]);
-        int y = Integer.parseInt(input[1]);
+        //int x = Integer.parseInt(input[0]);
+        //int y = Integer.parseInt(input[1]);
+        
+        if(e.getActionCommand().equals("Quit")){
+            var result = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?");
+            switch (result) {
+                case JOptionPane.YES_OPTION:
+                    System.exit(1);
+                    break;
+                case JOptionPane.NO_OPTION:
+                case JOptionPane.CANCEL_OPTION:
+                case JOptionPane.CLOSED_OPTION:
+                    JOptionPane.getRootFrame().dispose();
+                    break;
+            }
+
+        }
     }
 }
